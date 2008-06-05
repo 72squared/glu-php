a1a23d12a47dd11e3d3214266215920904a77ca9
John Loehrer <jloehrer@john-loehrers-computer.local>
2008-06-05 16:42:57 -0700

    an example of factory instantiation.

---
+++ examples/06_factory_instantiation/index.php
@@ -0,0 +1,35 @@
+<?
+// This demo shows how to build a very simple factory method of instantiation, and to keep
+// singleton instances of the object available. this example is not very useful but might give
+// ideas on how the pattern could be used for other real-life purposes.
+
+// include the grok class
+include '../../grok.php';
+
+// instantiate the grok and set the app
+$test = new Grok( dirname(__FILE__) . '/lib');
+
+// get the object once
+$a = $test->dispatch('singleton', array('class'=>'stdclass') );
+
+// print it out so we can inspect it.
+print "\nA: ";
+var_dump( $a );
+
+// now get the object again.
+$b = $test->dispatch('singleton',  array('class'=>'stdclass'));
+
+// print it out so we can see it's exact object id and compare with the earlier one.
+print "\nB: ";
+var_dump( $b );
+
+// compare.
+if( $a === $b ){
+    print "\nSingleton returned the same object both times.\n";
+} else {
+    print "\nSingleton returned a different object each time.\n";
+}
+
+// ALL DONE.
+print "\nDONE\n";
+// EOF
---
+++ examples/06_factory_instantiation/lib/singleton.php
@@ -0,0 +1,14 @@
+<?
+$class = $input->class;
+
+
+// generate a safe key based on the filename.
+$key = 'lib' . md5(__FILE__ . $class);
+
+// check to see if the object exists yet.
+if( $this->$key instanceof $class ) return $this->$key;
+
+// return the newly instantiated object object.
+return $this->$key = new $class;
+
+// EOF
