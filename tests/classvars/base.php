c1b50f1be916d14cceb50ad8fc23e6345ff5660c
John Loehrer <jloehrer@local.gaiaonline.com>
2008-06-06 12:22:56 -0700

    adding tests for the class variable accessors and manipulators.

---
+++ tests/classvars/base.php
@@ -0,0 +1,87 @@
+<?
+// include grok.
+include dirname( dirname( dirname(__FILE__) ) ) . DIRECTORY_SEPARATOR . 'grok.php';
+
+// set the current working directory.
+chdir( dirname(__FILE__)  );
+
+/**
+* Base class for testing the import method of Grok.
+*/
+class Grok_ClassVars_Test extends Snap_UnitTestCase {
+   
+   /**
+    * The grok object that we are testing
+    * @type Grok
+    */
+    protected $grok;
+    
+   /**
+    * the return value of Grok::import called in setup
+    * @type mixed
+    */
+    protected $input = array();
+    
+   /**
+    * a keyed array of the outcome of all the input set operations
+    * @type mixed
+    */
+    protected $result_set = array();
+    
+   /**
+    * a keyed array of the outcome of all the input get operations
+    * @type mixed
+    */
+    protected $result_get = array();
+    
+   /**
+    * a keyed array of the outcome of all the input isset operations
+    * @type mixed
+    */
+    protected $result_isset = array();
+    
+   /**
+    * a keyed array of the outcome of all the input get operations after the key has been unset.
+    * @type mixed
+    */
+    protected $result_unset = array();
+    
+    
+    
+   /**
+    * the argument we pass to Grok::__construct
+    * @type mixed
+    */
+    protected $arg = NULL;
+    
+   /**
+    * The base setup method.
+    * Here, we instantiate a grok, then export before and after dispatch.
+    *
+    */
+    public function setup() {
+        $this->grok = new Grok;
+        foreach( array('result_set', 'result_get', 'result_isset', 'result_unset') as $key ){
+            $this->$key = array();
+        }
+        if( ! is_array( $this->input ) ) $this->input = array();
+        foreach( $this->input as $k=>$v ){
+            $this->result_set[ $k ] = $this->grok->$k = $v;
+            $this->result_isset[ $k ] = isset( $this->grok->$k );
+            $this->result_get[ $k ] = $this->grok->$k;
+            unset( $this->grok->$k );
+            $this->result_unset[ $k ] = $this->grok->$k;
+        }
+    }
+    
+   /**
+    *Do some cleanup.
+    */
+    public function tearDown() {
+        foreach( array('grok', 'input', 'result_set', 'result_get', 'result_isset', 'result_unset') as $key ){
+            unset( $this->$key );
+        }
+    }
+}
+
+// EOF
---
+++ tests/classvars/string.stest.php
@@ -0,0 +1,36 @@
+<?
+include dirname(__FILE__) . DIRECTORY_SEPARATOR . 'base.php';
+
+class Grok_ClassVars_String_Test extends Grok_ClassVars_Test {
+
+    public function setup() {
+        $this->input = array('a'=>'string1', 'coMpLexKey'=>'b', 'fun'=>'run' );
+        parent::setup();
+    }
+    
+    public function test_Set(){
+        return $this->assertEqual( $this->input, $this->result_set );
+    }
+    
+    public function test_Get(){
+        return $this->assertEqual( $this->input, $this->result_get );
+    }
+    
+    public function test_Isset(){
+        $input = array();
+        foreach( array_keys( $this->input ) as $k) $input[ $k ] = TRUE;
+        return $this->assertEqual( $input, $this->result_isset );
+    }
+    
+    public function test_Unset(){
+        $input = array();
+        foreach( array_keys( $this->input ) as $k) $input[ $k ] = NULL;
+        return $this->assertEqual( $input, $this->result_unset );
+    }
+    
+    public function test_NonExistent(){
+        return $this->assertNull( $this->grok->non_existent );
+    }
+}
+
+// EOF
