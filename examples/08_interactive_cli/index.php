acc7bd5b53b96e10d8555e3bb4b65acd8d2bf4f8
John Loehrer <jloehrer@local.gaiaonline.com>
2008-06-07 17:38:03 -0700

    cleaning up the error handling for non-cli clients.

--- examples/08_interactive_cli/index.php
+++ examples/08_interactive_cli/index.php
@@ -1,6 +1,12 @@
 <?
 // an example of a CLI app.
 
+// make sure we are running from cli.
+if( ! is_resource( STDIN ) ){
+    // exit with an error message for the web browser.
+    die('<h1>Please run this from CLI.</h1><h2>Does not work in browser.</h2>');
+}
+
 // set the current working directory.
 chdir( dirname(__FILE__) );
 
@@ -9,18 +15,6 @@ include '../../grok.php';
 
 // create the app runner.
 $runner = new Grok('app');
-
-// make sure we are running from cli.
-if( ! is_resource( STDIN ) ){
-    // attach an exception
-    $runner->exception = '<h1>Please run this from CLI. does not work in browser</h1>';
-    
-    // dispatch the error view
-    $runner->dispatch('error');
-    
-    // all done.
-    exit;
-}
 
 // print out the help screen.
 $runner->dispatch('help' );
