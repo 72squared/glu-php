e9a2d8a2d28b83fd681a6058e0d246a1ee07285a
John Loehrer <jloehrer@john-loehrers-computer.local>
2008-06-10 08:14:09 -0700

    cleaning up some comments in some tests. the comments aren't really needed in this case because i write the tests so succinctly that the comments are more likely to be misleading than simply reading the test for yourself.

--- examples/01_simple_dispatch/tests/main.stest.php
+++ examples/01_simple_dispatch/tests/main.stest.php
@@ -1,38 +1,11 @@
 <?
-// set the current working directory.
 chdir( dirname(__FILE__)  );
-
-// include grok.
 include '../grok.php';
 
-/**
-* Base class for testing the import method of Grok.
-*/
 class Grok_SimpleDispatch_Main_Test extends Snap_UnitTestCase {
-    
-   /**
-    * the return value of Grok::import called in setup
-    * @type mixed
-    */
     protected $result;
-    
-   /**
-    * if an exception gets thrown in the setup, it is attached here.
-    * @type Exception / null
-    */
     protected $exception;
-    
-   /**
-    * the message of the exception, if one is thrown in setup.
-    * @type string / null
-    */
     protected $exception_message;
-    
-   /**
-    * The base setup method.
-    * Here, we instantiate a grok, then export before and after dispatch.
-    *
-    */
     public function setup() {
         try {
             ob_start();
