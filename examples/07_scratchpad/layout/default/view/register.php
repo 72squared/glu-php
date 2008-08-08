8b42bdb8946f7c20cfc4c72973a62147e9ceff87
John Loehrer <jloehrer@john-loehrers-computer.local>
2008-08-08 16:32:27 -0700

    found a few pages with some leftover unchanged vars

--- examples/07_scratchpad/layout/default/view/register.php
+++ examples/07_scratchpad/layout/default/view/register.php
@@ -1,6 +1,6 @@
 <?php
 $d = $this->dispatch(ROOT_DIR . 'action/register');
-if( $d->user->user_id ) return $this->dispatch( ROOT_DIR . 'app/display');
+if( $d->user->user_id ) return $this->dispatch( dirname(__FILE__) . '/display');
 $tpl = dirname(dirname(__FILE__)) . '/tpl/';
 $d->dispatch($tpl . 'header'); 
 $d->dispatch($tpl . 'registerform');
