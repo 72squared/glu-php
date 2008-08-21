435758e12595980b07abdf4cbf05018232a30783
John Loehrer <jloehrer@john-loehrers-computer.local>
2008-08-21 10:44:54 -0700

    moving some files around

--- examples/07_scratchpad/class/dir.php
+++ examples/07_scratchpad/class/dir.php
@@ -4,7 +4,7 @@ class dir extends Grok {
         parent::__construct( $data );
         $this->ROOT = dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR;
         $this->ACTION = $this->ROOT . 'action' . DIRECTORY_SEPARATOR;
-        $this->HTPASSWD = $this->ROOT . 'htpasswd' . DIRECTORY_SEPARATOR;
+        $this->SECRET = $this->ROOT . 'secret' . DIRECTORY_SEPARATOR;
         $this->ROUTE = $this->ROOT . 'route' . DIRECTORY_SEPARATOR . 'enabled' . DIRECTORY_SEPARATOR;
         $this->STATIC = $this->ROOT . 'static' . DIRECTORY_SEPARATOR;
         $this->TPL = $this->ROOT . 'tpl' . DIRECTORY_SEPARATOR;
--- examples/07_scratchpad/modules/available/authdigest.php
+++ examples/07_scratchpad/modules/available/authdigest.php
@@ -1,7 +1,7 @@
 <?php
 $this->digest = $this->server->PHP_AUTH_DIGEST;
 $this->realm = 'Scratchpad Admin Area';
-$this->htpasswd = $this->dir->ROOT . 'htpasswd/cleartext';
+$this->htpasswd = $this->dir->DIR_SECRET . '/digest_cleartext';
 
 try {
     $this->dispatch($this->dir->ACTION . 'authdigest');
